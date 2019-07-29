e7270947bb173c5c7dcfe438494cc7d2c701bc85
Christie Rice <8483753+crice100@users.noreply.github.com>
2019-07-29 10:20:36 -0400

    REVMI-354 Add view_courseware permission

--- lms/djangoapps/courseware/permissions.py
+++ lms/djangoapps/courseware/permissions.py
@@ -6,4 +6,7 @@ from bridgekeeper import perms
 from .rules import HasAccessRule
 
 VIEW_COURSE_HOME = 'courseware.view_course_home'
+VIEW_COURSEWARE = 'courseware.view_courseware'
+
 perms[VIEW_COURSE_HOME] = HasAccessRule('load')
+perms[VIEW_COURSEWARE] = HasAccessRule('load')
