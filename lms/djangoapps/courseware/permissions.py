4ec97618e472a79a30c81b65d46f13f22a02c048
Christie Rice <8483753+crice100@users.noreply.github.com>
2019-07-30 14:58:14 -0400

    REVMI-354 Add view_xqa permission

--- lms/djangoapps/courseware/permissions.py
+++ lms/djangoapps/courseware/permissions.py
@@ -6,4 +6,7 @@ from bridgekeeper import perms
 from .rules import HasAccessRule
 
 VIEW_COURSE_HOME = 'courseware.view_course_home'
+VIEW_XQA_INTERFACE = 'courseware.view_xqa_interface'
+
 perms[VIEW_COURSE_HOME] = HasAccessRule('load')
+perms[VIEW_XQA_INTERFACE] = HasAccessRule('staff')
