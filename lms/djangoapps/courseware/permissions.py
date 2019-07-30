d957781ec9703052b3c090b3bd68fdb8e5710793
Matthew Piatetsky <mpiatetsky@edx.org>
2019-07-30 15:51:27 -0400

    can access course rule

--- lms/djangoapps/courseware/permissions.py
+++ lms/djangoapps/courseware/permissions.py
@@ -7,3 +7,7 @@ from .rules import HasAccessRule
 
 VIEW_COURSE_HOME = 'courseware.view_course_home'
 perms[VIEW_COURSE_HOME] = HasAccessRule('load')
+ACCESS_COURSE = 'courseware.access_course'
+perms[ACCESS_COURSE] = HasAccessRule('staff')
+CAN_MASQUERADE_AS_STUDENT = 'courseware.can_masquerade_as_student'
+perms[CAN_MASQUERADE_AS_STUDENT] = HasAccessRule('staff')
--- lms/djangoapps/courseware/views/index.py
+++ lms/djangoapps/courseware/views/index.py
@@ -32,6 +32,7 @@ from web_fragments.fragment import Fragment
 from edxmako.shortcuts import render_to_response, render_to_string
 from lms.djangoapps.courseware.courses import allow_public_access
 from lms.djangoapps.courseware.exceptions import CourseAccessRedirect
+from lms.djangoapps.courseware.permissions import ACCESS_COURSE, CAN_MASQUERADE_AS_STUDENT
 from lms.djangoapps.experiments.utils import get_experiment_user_metadata_context
 from lms.djangoapps.gating.api import get_entrance_exam_score_ratio, get_entrance_exam_usage_key
 from lms.djangoapps.grades.api import CourseGradeFactory
@@ -152,7 +153,8 @@ class CoursewareIndex(View):
                         # If the user is considered enrolled show the default XBlock student_view.
                         pass
 
-                self.is_staff = has_access(request.user, 'staff', self.course)
+                self.is_staff = (request.user.has_perm(ACCESS_COURSE, self.course.id) and
+                                 request.user.has_perm(CAN_MASQUERADE_AS_STUDENT, self.course.id))
                 self._setup_masquerade_for_effective_user()
 
                 return self.render(request)
