1006359cacfc33460df0ab3f8a5f4f5b652a47e2
Matthew Piatetsky <mpiatetsky@edx.org>
2019-07-30 17:20:23 -0400

    add view_ccx_coach_dashboard permission

--- lms/djangoapps/ccx/urls.py
+++ lms/djangoapps/ccx/urls.py
@@ -5,19 +5,28 @@ from __future__ import absolute_import
 
 from django.conf.urls import url
 
-import ccx.views
+from lms.djangoapps.ccx.views import (
+    create_ccx,
+    ccx_gradebook,
+    ccx_grades_csv,
+    ccx_schedule,
+    ccx_students_management,
+    dashboard,
+    save_ccx,
+    set_grading_policy
+)
 
 urlpatterns = [
-    url(r'^ccx_coach$', ccx.views.dashboard, name='ccx_coach_dashboard'),
-    url(r'^create_ccx$', ccx.views.create_ccx, name='create_ccx'),
-    url(r'^save_ccx$', ccx.views.save_ccx, name='save_ccx'),
-    url(r'^ccx_schedule$', ccx.views.ccx_schedule, name='ccx_schedule'),
-    url(r'^ccx-manage-students$', ccx.views.ccx_students_management, name='ccx-manage-students'),
+    url(r'^ccx_coach$', dashboard, name='ccx_coach_dashboard'),
+    url(r'^create_ccx$', create_ccx, name='create_ccx'),
+    url(r'^save_ccx$', save_ccx, name='save_ccx'),
+    url(r'^ccx_schedule$', ccx_schedule, name='ccx_schedule'),
+    url(r'^ccx-manage-students$', ccx_students_management, name='ccx-manage-students'),
 
     # Grade book
-    url(r'^ccx_gradebook$', ccx.views.ccx_gradebook, name='ccx_gradebook'),
-    url(r'^ccx_gradebook/(?P<offset>[0-9]+)$', ccx.views.ccx_gradebook, name='ccx_gradebook'),
+    url(r'^ccx_gradebook$', ccx_gradebook, name='ccx_gradebook'),
+    url(r'^ccx_gradebook/(?P<offset>[0-9]+)$', ccx_gradebook, name='ccx_gradebook'),
 
-    url(r'^ccx_grades.csv$', ccx.views.ccx_grades_csv, name='ccx_grades_csv'),
-    url(r'^ccx_set_grading_policy$', ccx.views.set_grading_policy, name='ccx_set_grading_policy'),
+    url(r'^ccx_grades.csv$', ccx_grades_csv, name='ccx_grades_csv'),
+    url(r'^ccx_set_grading_policy$', set_grading_policy, name='ccx_set_grading_policy'),
 ]
