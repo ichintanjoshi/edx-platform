45fe8afdc7281f1e0a60b4a76f21d3637670a12d
Christie Rice <8483753+crice100@users.noreply.github.com>
2019-07-30 14:39:49 -0400

    REVMI-354 Add edit_bookmark permission

--- lms/djangoapps/courseware/permissions.py
+++ lms/djangoapps/courseware/permissions.py
@@ -5,5 +5,8 @@ Permission definitions for the courseware djangoapp
 from bridgekeeper import perms
 from .rules import HasAccessRule
 
+EDIT_BOOKMARK = 'courseware.edit_bookmark'
 VIEW_COURSE_HOME = 'courseware.view_course_home'
+
+perms[EDIT_BOOKMARK] = HasAccessRule('staff')
 perms[VIEW_COURSE_HOME] = HasAccessRule('load')
